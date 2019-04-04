9ce52bfa54bf6897a49619b8d1084e8535b1134b
Vlad Glabai <vglabai@examsoft.com>
2019-04-03 20:57:55 -0500

    implement dictation bug demo

--- DictationBug/ViewController.swift
+++ DictationBug/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
