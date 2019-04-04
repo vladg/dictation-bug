e66656507e72e22a41f5b1b8fc4770dc137e270d
Vlad Glabai <vglabai@examsoft.com>
2019-04-03 20:33:08 -0500

    create single view app

---
+++ DictationBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DictationBug
+//
+//  Created by Vlad Glabai on 4/3/19.
+//  Copyright © 2019 ExamSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
